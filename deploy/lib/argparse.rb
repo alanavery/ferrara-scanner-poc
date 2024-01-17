57b3aa32e8ec94d1c822f530a0a86e97c0c94397
Erlington Espinoza Cardoza <ecardoza@ampagency.com>
2024-01-17 10:53:26 -0600

    [FCAN-44] Fix lint errors (#2)
    * [FCAN-44] Added read-all top level permissions
    
    * [FCAN-44] Fixed triple-slashes and empty reference in viteconfig
    
    * [FCAN-44] Fixed triple-slashes and empty reference in viteconfig
    
    * [FCAN-44] Addressed empty reference in tsconfig.json and remove files with duplicated config
    
    * [FCAN-44] Addressed kics linter issues
    
    * [FCAN-44] Disabled TSX and TYPESCRIPT linter
    
    * [MegaLinter] Apply linters fixes
    
    * [FCAN-44] Disable top-level permissions completely in workflows
    
    * [FCAN-44] Applied fixes suggested by rubocop
    
    * [FCAN-44] Added missing newline to .eslintrc.cjs
    
    * [FCAN-44] Reduced ABC Size and Method Lenght to address ruby linter errors
    
    * [FCAN-44] Added missing permissions in build job
    
    * [FCAN-44] Added missing newline
    
    * [MegaLinter] Apply linters fixes
    
    * Empty commit
    
    ---------
    
    Co-authored-by: erlingtonjcardoza <erlingtonjcardoza@users.noreply.github.com>
    Co-authored-by: Götz <git.nte0w@8shield.net>

--- deploy/lib/argparse.rb
+++ deploy/lib/argparse.rb
@@ -2,36 +2,46 @@
 
 require 'optparse'
 
-# rubocop:disable Metrics/MethodLength,Metrics/AbcSize
 def parse_args(intro = nil)
   options = {}
   ARGV << '-h' if ARGV.empty?
 
   OptionParser.new do |opts|
-    opts.banner = intro
-    opts.banner << "Usage: #{$PROGRAM_NAME} [OPTIONS ...]"
-
-    opts.on('-e', '--environment ENVIRONMENT', String, 'Target environment') do |opt|
-      options[:environment] = opt
-    end
-
-    if $PROGRAM_NAME.include? 'release'
-      opts.on('-p', '--push', 'Push the release to remote') do
-        options[:push_release] = 'true'
-      end
-    end
-
-    opts.on_tail('-h', '--help', 'Show this message') do
-      puts opts
-      exit
-    end
+    configure_banner(opts, intro)
+    configure_environment_option(opts, options)
+    configure_push_option(opts, options) if $PROGRAM_NAME.include? 'release'
+    configure_help_option(opts)
   end.parse!
 
-  raise OptionParser::MissingArgument unless options.length.positive?
-
-  ENV['ENVIRONMENT']  = options[:environment]
-  ENV['PUSH_RELEASE'] = options[:push_release]
+  validate_options(options)
 
   options
 end
-# rubocop:enable Metrics/MethodLength,Metrics/AbcSize
+
+def configure_banner(opts, intro)
+  opts.banner = intro
+  opts.banner << "Usage: #{$PROGRAM_NAME} [OPTIONS ...]"
+end
+
+def configure_environment_option(opts, options)
+  opts.on('-e', '--environment ENVIRONMENT', String, 'Target environment') do |opt|
+    options[:environment] = opt
+  end
+end
+
+def configure_push_option(opts, options)
+  opts.on('-p', '--push', 'Push the release to remote') do
+    options[:push_release] = 'true'
+  end
+end
+
+def configure_help_option(opts)
+  opts.on_tail('-h', '--help', 'Show this message') do
+    puts opts
+    exit
+  end
+end
+
+def validate_options(options)
+  raise OptionParser::MissingArgument unless options.length.positive?
+end
