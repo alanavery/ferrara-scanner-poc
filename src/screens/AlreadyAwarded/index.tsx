8cfb996849b3d5cd87b587e1cd0c619765d45738
Daniel Araújo <daniel.araujorn01@gmail.com>
2024-02-15 14:24:35 -0300

    FCAN-72 (#13)

--- src/screens/AlreadyAwarded/index.tsx
+++ src/screens/AlreadyAwarded/index.tsx
@@ -1,5 +1,4 @@
 import { Layout } from "../../components/Layout";
-import { Offer } from "../../components/Offer";
 import { Text } from "../../components/Text";
 import { Title } from "../../components/Title";
 import styles from "./index.module.css";
@@ -9,13 +8,8 @@ export const AlreadyAwarded = () => (
     <div className={styles.container}>
       <Title>Today's prize is already awarded</Title>
       <Text>
-        Come back tomorrow for another chance to WIN sweet
-        summer prizes!
+        Come back tomorrow for another chance to WIN sweet summer prizes!
       </Text>
-      <Offer
-        title="Special Offer"
-        description="Check your email for a special offer on your next candy purchase!"
-      />
     </div>
   </Layout>
 );
